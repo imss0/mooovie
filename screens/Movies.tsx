0c9b83c611181461fca082f5a528bceeb1f3d129
sohyun <shlim0410@gmail.com>
2023-05-01 21:21:35 +0100

    feat: Movies tab header done (Now Playing Movies & show star rating)

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -2,10 +2,16 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
 import styled from "styled-components/native";
 import Swiper from "react-native-swiper";
-import { ActivityIndicator, Dimensions, StyleSheet } from "react-native";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
 import { API_KEY } from "@env";
 import { useState, useEffect } from "react";
 import { BlurView } from "expo-blur";
+import { FontAwesome } from "@expo/vector-icons";
 
 // Create styled components
 const Container = styled.ScrollView`
@@ -24,7 +30,48 @@ const Loader = styled.View`
 
 const BackgroundImg = styled.Image``;
 
-const Title = styled.Text``;
+const Poster = styled.Image`
+  width: 110px;
+  height: 170px;
+  margin-left: 15px;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  font-weight: 700;
+  margin-bottom: 5px;
+  color: ${(props) => props.theme.textColor};
+`;
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextWrapper = styled.ScrollView`
+  width: 60%;
+  margin-left: 20px;
+  margin-right: 15px;
+  height: 170px;
+  overflow: scroll;
+`;
+
+const OverView = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  opacity: 0.8;
+  margin-top: 5px;
+`;
+
+const Vote = styled(Title)`
+  font-size: 14px;
+  margin-right: 10px;
+`;
+
+const VoteContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
 
 // Utility function & variable
 const makeImgPath = (img: string, width: string = "w500") =>
@@ -32,10 +79,25 @@ const makeImgPath = (img: string, width: string = "w500") =>
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
+const getStars = (rating: number) => {
+  rating = Math.round(rating) / 2;
+  let output = [];
+  for (var i = rating; i >= 1; i--) output.push("full");
+  //<FontAwesome name="star" size={24} color="yellow" />&nbsp;
+  if (i == 0.5) output.push("half");
+  //<FontAwesome name="star-half-full" size={24} color="yellow" />&nbsp;
+  for (let i = 5 - rating; i >= 1; i--) output.push("empty");
+  // <FontAwesome name="star-o" size={24} color="yellow" /> & nbsp;
+
+  return output;
+};
+
 // Main function
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState<any[]>([]);
+  const isDark = useColorScheme() === "dark";
+
   const getNowPlaying = async () => {
     const { results } = await (
       await fetch(
@@ -58,9 +120,11 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
     <Container>
       <Swiper
         loop
+        horizontal
         autoplay={true}
         autoplayTimeout={3}
         showsPagination={false}
+        showsButtons={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlaying.map((movie) => (
@@ -69,8 +133,60 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
               source={{ uri: makeImgPath(movie.backdrop_path) }}
               style={StyleSheet.absoluteFill}
             />
-            <BlurView intensity={50} style={StyleSheet.absoluteFill}>
-              <Title>{movie.original_title}</Title>
+            <BlurView
+              tint={isDark ? "dark" : "light"}
+              intensity={50}
+              style={StyleSheet.absoluteFill}
+            >
+              <Wrapper>
+                <Poster
+                  source={{ uri: makeImgPath(movie.poster_path) }}
+                ></Poster>
+                <TextWrapper>
+                  <Title>{movie.original_title}</Title>
+                  {movie.vote_average > 0 ? (
+                    <VoteContainer>
+                      <Vote>{movie.vote_average}</Vote>
+                      <Vote>
+                        {getStars(movie.vote_average).map((item, index) => {
+                          if (item === "full") {
+                            return (
+                              <FontAwesome
+                                name="star"
+                                size={14}
+                                color="#fbbf24"
+                                key={index}
+                              />
+                            );
+                          }
+                          if (item === "half") {
+                            return (
+                              <FontAwesome
+                                name="star-half-full"
+                                size={14}
+                                color="#fbbf24"
+                                key={index}
+                              />
+                            );
+                          }
+
+                          if (item === "empty") {
+                            return (
+                              <FontAwesome
+                                name="star-o"
+                                size={14}
+                                color="#fbbf24"
+                                key={index}
+                              />
+                            );
+                          }
+                        })}
+                      </Vote>
+                    </VoteContainer>
+                  ) : null}
+                  <OverView>{movie.overview}</OverView>
+                </TextWrapper>
+              </Wrapper>
             </BlurView>
           </View>
         ))}
